3764f93bdcf22f1650f6edffe333ed4ec4900baf
Rowanda <rwandodo@hotmail.com>
2020-12-14 11:29:43 +0300

    Update predict_image.py

--- predict_image.py
+++ predict_image.py
@@ -6,7 +6,11 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 model = load_model('model_vgg19.h5')
-img = image.load_img('val/PNEUMONIA/person1946_bacteria_4874.jpeg', target_size=(224, 224))
+
+
+#img = image.load_img('Datasets/chest_xray/chest_xray/val/PNEUMONIA/person1946_bacteria_4874.jpeg', target_size=(224, 224))
+img = image.load_img('Datasets/chest_xray/chest_xray/val/NORMAL/NORMAL2-IM-1427-0001.jpeg', target_size=(224, 224))
+
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 img_data = preprocess_input(x)
